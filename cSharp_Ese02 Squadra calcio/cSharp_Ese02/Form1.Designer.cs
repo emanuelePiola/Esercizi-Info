9375345596780081fe9073d56fc19ecbc28fa6c6
emanuelePiola <e.piola.2730@vallauri.edu>
2023-11-24 20:47:37 +0100

    Ese02 Squadre calcio added & started

---
+++ cSharp_Ese02 Squadra calcio/cSharp_Ese02/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace cSharp_Ese02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nomeA = new System.Windows.Forms.Label();
+            this.nomeC = new System.Windows.Forms.Label();
+            this.nomeB = new System.Windows.Forms.Label();
+            this.cittaA = new System.Windows.Forms.TextBox();
+            this.cittaC = new System.Windows.Forms.TextBox();
+            this.cittaB = new System.Windows.Forms.TextBox();
+            this.giocateA = new System.Windows.Forms.TextBox();
+            this.giocateC = new System.Windows.Forms.TextBox();
+            this.giocateB = new System.Windows.Forms.TextBox();
+            this.btnPareggiC = new System.Windows.Forms.Button();
+            this.btnPareggiB = new System.Windows.Forms.Button();
+            this.btnPareggiA = new System.Windows.Forms.Button();
+            this.btnVittorieC = new System.Windows.Forms.Button();
+            this.btnVittorieB = new System.Windows.Forms.Button();
+            this.btnVittorieA = new System.Windows.Forms.Button();
+            this.btnCreaC = new System.Windows.Forms.Button();
+            this.btnCreaB = new System.Windows.Forms.Button();
+            this.btnCreaA = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nomeA
+            // 
+            this.nomeA.AutoSize = true;
+            this.nomeA.Location = new System.Drawing.Point(12, 15);
+            this.nomeA.Name = "nomeA";
+            this.nomeA.Size = new System.Drawing.Size(43, 13);
+            this.nomeA.TabIndex = 0;
+            this.nomeA.Text = "Atlante:";
+            // 
+            // nomeC
+            // 
+            this.nomeC.AutoSize = true;
+            this.nomeC.Location = new System.Drawing.Point(12, 67);
+            this.nomeC.Name = "nomeC";
+            this.nomeC.Size = new System.Drawing.Size(36, 13);
+            this.nomeC.TabIndex = 1;
+            this.nomeC.Text = "Virtus:";
+            // 
+            // nomeB
+            // 
+            this.nomeB.AutoSize = true;
+            this.nomeB.Location = new System.Drawing.Point(12, 41);
+            this.nomeB.Name = "nomeB";
+            this.nomeB.Size = new System.Drawing.Size(63, 13);
+            this.nomeB.TabIndex = 2;
+            this.nomeB.Text = "Macedonia:";
+            // 
+            // cittaA
+            // 
+            this.cittaA.BackColor = System.Drawing.Color.Yellow;
+            this.cittaA.Location = new System.Drawing.Point(95, 12);
+            this.cittaA.Name = "cittaA";
+            this.cittaA.Size = new System.Drawing.Size(100, 20);
+            this.cittaA.TabIndex = 3;
+            // 
+            // cittaC
+            // 
+            this.cittaC.BackColor = System.Drawing.Color.Yellow;
+            this.cittaC.Location = new System.Drawing.Point(95, 64);
+            this.cittaC.Name = "cittaC";
+            this.cittaC.Size = new System.Drawing.Size(100, 20);
+            this.cittaC.TabIndex = 4;
+            // 
+            // cittaB
+            // 
+            this.cittaB.BackColor = System.Drawing.Color.Yellow;
+            this.cittaB.Location = new System.Drawing.Point(95, 38);
+            this.cittaB.Name = "cittaB";
+            this.cittaB.Size = new System.Drawing.Size(100, 20);
+            this.cittaB.TabIndex = 5;
+            // 
+            // giocateA
+            // 
+            this.giocateA.BackColor = System.Drawing.Color.Magenta;
+            this.giocateA.Location = new System.Drawing.Point(201, 12);
+            this.giocateA.Name = "giocateA";
+            this.giocateA.Size = new System.Drawing.Size(100, 20);
+            this.giocateA.TabIndex = 6;
+            // 
+            // giocateC
+            // 
+            this.giocateC.BackColor = System.Drawing.Color.Magenta;
+            this.giocateC.Location = new System.Drawing.Point(201, 64);
+            this.giocateC.Name = "giocateC";
+            this.giocateC.Size = new System.Drawing.Size(100, 20);
+            this.giocateC.TabIndex = 7;
+            // 
+            // giocateB
+            // 
+            this.giocateB.BackColor = System.Drawing.Color.Magenta;
+            this.giocateB.Location = new System.Drawing.Point(201, 38);
+            this.giocateB.Name = "giocateB";
+            this.giocateB.Size = new System.Drawing.Size(100, 20);
+            this.giocateB.TabIndex = 8;
+            // 
+            // btnPareggiC
+            // 
+            this.btnPareggiC.Location = new System.Drawing.Point(207, 157);
+            this.btnPareggiC.Name = "btnPareggiC";
+            this.btnPareggiC.Size = new System.Drawing.Size(90, 23);
+            this.btnPareggiC.TabIndex = 26;
+            this.btnPareggiC.Text = "Pareggi C";
+            this.btnPareggiC.UseVisualStyleBackColor = true;
+            // 
+            // btnPareggiB
+            // 
+            this.btnPareggiB.Location = new System.Drawing.Point(111, 157);
+            this.btnPareggiB.Name = "btnPareggiB";
+            this.btnPareggiB.Size = new System.Drawing.Size(90, 23);
+            this.btnPareggiB.TabIndex = 25;
+            this.btnPareggiB.Text = "Pareggi B";
+            this.btnPareggiB.UseVisualStyleBackColor = true;
+            // 
+            // btnPareggiA
+            // 
+            this.btnPareggiA.Location = new System.Drawing.Point(15, 157);
+            this.btnPareggiA.Name = "btnPareggiA";
+            this.btnPareggiA.Size = new System.Drawing.Size(90, 23);
+            this.btnPareggiA.TabIndex = 24;
+            this.btnPareggiA.Text = "Pareggi A";
+            this.btnPareggiA.UseVisualStyleBackColor = true;
+            // 
+            // btnVittorieC
+            // 
+            this.btnVittorieC.Location = new System.Drawing.Point(207, 128);
+            this.btnVittorieC.Name = "btnVittorieC";
+            this.btnVittorieC.Size = new System.Drawing.Size(90, 23);
+            this.btnVittorieC.TabIndex = 23;
+            this.btnVittorieC.Text = "Vinte C";
+            this.btnVittorieC.UseVisualStyleBackColor = true;
+            // 
+            // btnVittorieB
+            // 
+            this.btnVittorieB.Location = new System.Drawing.Point(111, 128);
+            this.btnVittorieB.Name = "btnVittorieB";
+            this.btnVittorieB.Size = new System.Drawing.Size(90, 23);
+            this.btnVittorieB.TabIndex = 22;
+            this.btnVittorieB.Text = "Vinte B";
+            this.btnVittorieB.UseVisualStyleBackColor = true;
+            // 
+            // btnVittorieA
+            // 
+            this.btnVittorieA.Location = new System.Drawing.Point(15, 128);
+            this.btnVittorieA.Name = "btnVittorieA";
+            this.btnVittorieA.Size = new System.Drawing.Size(90, 23);
+            this.btnVittorieA.TabIndex = 21;
+            this.btnVittorieA.Text = "Vinte A";
+            this.btnVittorieA.UseVisualStyleBackColor = true;
+            // 
+            // btnCreaC
+            // 
+            this.btnCreaC.Location = new System.Drawing.Point(207, 99);
+            this.btnCreaC.Name = "btnCreaC";
+            this.btnCreaC.Size = new System.Drawing.Size(90, 23);
+            this.btnCreaC.TabIndex = 20;
+            this.btnCreaC.Text = "Crea C";
+            this.btnCreaC.UseVisualStyleBackColor = true;
+            this.btnCreaC.Click += new System.EventHandler(this.btnCreaC_Click);
+            // 
+            // btnCreaB
+            // 
+            this.btnCreaB.Location = new System.Drawing.Point(111, 99);
+            this.btnCreaB.Name = "btnCreaB";
+            this.btnCreaB.Size = new System.Drawing.Size(90, 23);
+            this.btnCreaB.TabIndex = 19;
+            this.btnCreaB.Text = "Crea B";
+            this.btnCreaB.UseVisualStyleBackColor = true;
+            this.btnCreaB.Click += new System.EventHandler(this.btnCreaB_Click);
+            // 
+            // btnCreaA
+            // 
+            this.btnCreaA.Location = new System.Drawing.Point(15, 99);
+            this.btnCreaA.Name = "btnCreaA";
+            this.btnCreaA.Size = new System.Drawing.Size(90, 23);
+            this.btnCreaA.TabIndex = 18;
+            this.btnCreaA.Text = "Crea A";
+            this.btnCreaA.UseVisualStyleBackColor = true;
+            this.btnCreaA.Click += new System.EventHandler(this.btnCreaA_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(316, 192);
+            this.Controls.Add(this.btnPareggiC);
+            this.Controls.Add(this.btnPareggiB);
+            this.Controls.Add(this.btnPareggiA);
+            this.Controls.Add(this.btnVittorieC);
+            this.Controls.Add(this.btnVittorieB);
+            this.Controls.Add(this.btnVittorieA);
+            this.Controls.Add(this.btnCreaC);
+            this.Controls.Add(this.btnCreaB);
+            this.Controls.Add(this.btnCreaA);
+            this.Controls.Add(this.giocateB);
+            this.Controls.Add(this.giocateC);
+            this.Controls.Add(this.giocateA);
+            this.Controls.Add(this.cittaB);
+            this.Controls.Add(this.cittaC);
+            this.Controls.Add(this.cittaA);
+            this.Controls.Add(this.nomeB);
+            this.Controls.Add(this.nomeC);
+            this.Controls.Add(this.nomeA);
+            this.Name = "Form1";
+            this.Text = "Calcio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nomeA;
+        private System.Windows.Forms.Label nomeC;
+        private System.Windows.Forms.Label nomeB;
+        private System.Windows.Forms.TextBox cittaA;
+        private System.Windows.Forms.TextBox cittaC;
+        private System.Windows.Forms.TextBox cittaB;
+        private System.Windows.Forms.TextBox giocateA;
+        private System.Windows.Forms.TextBox giocateC;
+        private System.Windows.Forms.TextBox giocateB;
+        private System.Windows.Forms.Button btnPareggiC;
+        private System.Windows.Forms.Button btnPareggiB;
+        private System.Windows.Forms.Button btnPareggiA;
+        private System.Windows.Forms.Button btnVittorieC;
+        private System.Windows.Forms.Button btnVittorieB;
+        private System.Windows.Forms.Button btnVittorieA;
+        private System.Windows.Forms.Button btnCreaC;
+        private System.Windows.Forms.Button btnCreaB;
+        private System.Windows.Forms.Button btnCreaA;
+    }
+}
+
---
+++ cSharp_Ese02 Squadra calcio/cSharp_Ese02/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharp_Ese02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Squadre
+        {
+            public string nome;
+            public string citta;
+            public int giocate;
+            public int vinte;
+            public int pareggiate;
+        }
+
+        Squadre squadraA=new Squadre();
+        Squadre squadraB=new Squadre();
+        Squadre squadraC=new Squadre();
+
+        private void btnCreaA_Click(object sender, EventArgs e)
+        {
+            if (cittaA.Text.Length <= 0 || giocateA.Text.Length <= 0)
+            {
+                MessageBox.Show("Inserire le cordinate del punto che si vuole creare", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                squadraA.nome = nomeA.Text;
+                squadraA.citta = cittaA.Text;
+                squadraA.giocate = int.Parse(giocateA.Text);
+            }
+        }
+
+        private void btnCreaB_Click(object sender, EventArgs e)
+        {
+            if (cittaB.Text.Length <= 0 || giocateB.Text.Length <= 0)
+            {
+                MessageBox.Show("Inserire le cordinate del punto che si vuole creare", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                squadraB.nome = nomeB.Text;
+                squadraB.citta = cittaB.Text;
+                squadraB.giocate = int.Parse(giocateB.Text);
+            }
+        }
+
+        private void btnCreaC_Click(object sender, EventArgs e)
+        {
+            if (cittaC.Text.Length <= 0 || giocateC.Text.Length <= 0)
+            {
+                MessageBox.Show("Inserire le cordinate del punto che si vuole creare", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                squadraC.nome = nomeC.Text;
+                squadraC.citta = cittaC.Text;
+                squadraC.giocate = int.Parse(giocateC.Text);
+            }
+        }
+    }
+}
