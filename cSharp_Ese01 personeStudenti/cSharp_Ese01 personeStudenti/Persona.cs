8d1145f81fe2dd4af305cf2e7bbb2b324f01a6a1
unknown <e.piola.2730@vallauri.net>
2023-12-02 10:35:10 +0100

    Ese01 studente e persone added

---
+++ cSharp_Ese01 personeStudenti/cSharp_Ese01 personeStudenti/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Ese01_personeStudenti
+{
+    internal class Persona
+    {
+        protected string nome;
+        protected string cognome;
+        protected char sesso;
+        protected int eta;
+
+        public void Set(string name, string surname, char sex, int age)
+        {
+            nome = name;
+            cognome = surname;
+            sesso = sex;
+            eta = age;
+        }
+
+        public void Get(out string name,out string surname,out char sex,out int age)
+        {
+            name = nome;
+            surname = cognome;
+            sex = sesso;
+            age = eta;
+        }
+    }
+}
---
+++ cSharp_Ese01 personeStudenti/cSharp_Ese01 personeStudenti/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("cSharp_Ese01 personeStudenti")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cSharp_Ese01 personeStudenti")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("c1b5d2ee-c4b2-414f-ab94-329b00ad3444")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+// usando l'asterisco '*' come illustrato di seguito:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
