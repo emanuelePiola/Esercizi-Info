8382326ba7e77b5e28666a7cd1fcb6d3b9af0f20
unknown <e.piola.2730@vallauri.net>
2023-12-06 13:02:06 +0100

    Ese03 improved

--- cSharp_Ese03 scopa/cSharp_Ese03 scopa/Mano.cs
+++ cSharp_Ese03 scopa/cSharp_Ese03 scopa/Mano.cs
@@ -22,29 +22,6 @@ namespace cSharp_Ese03_scopa
 
         public Carta[] lettura()
         {
-            collezioneCarte = new Carta[40];
-            for (int i = 0; i < collezioneCarte.Length; i++)
-            { 
-                int value = rnd.Next(1, 11);
-                int n_suit= rnd.Next(0, 4);
-                string suit="";
-                switch(n_suit)
-                {
-                    case 0:
-                        suit = "Picche";
-                        break;
-                    case 1:
-                        suit = "Cuori";
-                        break;
-                    case 2:
-                        suit = "Fiori";
-                        break;
-                    case 3:
-                        suit = "Rombi";
-                        break;
-                }
-                collezioneCarte[i].scrittura(value, suit);
-            }
             return collezioneCarte;
         }
 
@@ -52,6 +29,38 @@ namespace cSharp_Ese03_scopa
         {
             int i = rnd.Next(0, cards.Length);
             return cards[i];
+        }
+
+        public Carta[] generaMazzo(int numCarte=40)
+        {
+            collezioneCarte = new Carta[numCarte];
+            int k = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < numCarte/4; j++)
+                {
+                    int value = i;
+                    int n_suit = j;
+                    string suit = "";
+                    switch (n_suit)
+                    {
+                        case 0:
+                            suit = "Picche";
+                            break;
+                        case 1:
+                            suit = "Cuori";
+                            break;
+                        case 2:
+                            suit = "Fiori";
+                            break;
+                        case 3:
+                            suit = "Rombi";
+                            break;
+                    }
+                    collezioneCarte[k++].scrittura(value, suit);
+                }
+            }
+            return collezioneCarte;
         }
     }
 }
