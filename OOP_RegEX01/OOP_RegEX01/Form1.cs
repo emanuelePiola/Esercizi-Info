44df4d16e677f7eb58a50aa0486a39a009258a67
unknown <e.piola.2730@vallauri.net>
2024-01-17 09:45:07 +0100

    oop-rege01 added

---
+++ OOP_RegEX01/OOP_RegEX01/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_RegEX01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string pat = "^{10,}[a-zA-Z]{4}[1-9]{3}[#,$,&]{1,}";
+            Regex r = new Regex(pat, RegexOptions.IgnoreCase);
+        }
+    }
+}
