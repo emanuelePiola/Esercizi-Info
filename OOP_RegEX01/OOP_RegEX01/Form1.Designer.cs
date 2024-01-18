ea1bfc3efb45f371e4cc77ca3c2eb8262934fe55
emanuelePiola <124259956+emanuelePiola@users.noreply.github.com>
2024-01-18 19:43:10 +0100

    OOP_RegEX01 done

--- OOP_RegEX01/OOP_RegEX01/Form1.Designer.cs
+++ OOP_RegEX01/OOP_RegEX01/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMatricola = new System.Windows.Forms.TextBox();
+            this.btnInvia = new System.Windows.Forms.Button();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matricola:";
+            // 
+            // txtMatricola
+            // 
+            this.txtMatricola.Location = new System.Drawing.Point(96, 43);
+            this.txtMatricola.Name = "txtMatricola";
+            this.txtMatricola.Size = new System.Drawing.Size(204, 26);
+            this.txtMatricola.TabIndex = 1;
+            // 
+            // btnInvia
+            // 
+            this.btnInvia.Location = new System.Drawing.Point(183, 295);
+            this.btnInvia.Name = "btnInvia";
+            this.btnInvia.Size = new System.Drawing.Size(117, 47);
+            this.btnInvia.TabIndex = 2;
+            this.btnInvia.Text = "INVIA";
+            this.btnInvia.UseVisualStyleBackColor = true;
+            this.btnInvia.Click += new System.EventHandler(this.btnInvia_Click);
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(96, 211);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(204, 26);
+            this.txtMail.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mail:";
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(96, 152);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(204, 26);
+            this.txtCognome.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cognome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(96, 95);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(204, 26);
+            this.txtNome.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nome:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(320, 375);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCognome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnInvia);
+            this.Controls.Add(this.txtMatricola);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form:";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMatricola;
+        private System.Windows.Forms.Button btnInvia;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- OOP_RegEX01/OOP_RegEX01/Form1.cs
+++ OOP_RegEX01/OOP_RegEX01/Form1.cs
@@ -16,9 +16,52 @@ namespace OOP_RegEX01
         public Form1()
         {
             InitializeComponent();
+        }
 
-            string pat = "^{10,}[a-zA-Z]{4}[1-9]{3}[#,$,&]{1,}";
-            Regex r = new Regex(pat, RegexOptions.IgnoreCase);
+        Regex regMatricola;
+        Regex regNomeCognome;
+        Regex regMail;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string patMatricola = @"^{10,}[a-zA-Z]{4}[1-9]{3}[#,$,&]{1,}";
+            string patNomeCognome = @"^{3,}";
+            string patMail = @"[\w,.]{3,}[@]{1}\w{3,}[.]{1}\w{2,}";
+
+            regMatricola = new Regex(patMatricola, RegexOptions.IgnoreCase);
+            regNomeCognome = new Regex(patNomeCognome, RegexOptions.IgnoreCase);
+            regMail = new Regex(patMail, RegexOptions.IgnoreCase);
+        }
+
+        private void btnInvia_Click(object sender, EventArgs e)
+        {
+
+            if (!regMatricola.IsMatch(txtMatricola.Text))
+            {
+                MessageBox.Show("Matricola NON valida!", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMatricola.Text = "";
+                return;
+            }
+            else if(!regNomeCognome.IsMatch(txtNome.Text))
+            {
+                MessageBox.Show("Nome NON valido!", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNome.Text = "";
+                return;
+            }
+            else if (!regNomeCognome.IsMatch(txtCognome.Text))
+            {
+                MessageBox.Show("Cognome NON valido!", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtCognome.Text = "";
+                return;
+            }
+            else if (!regMail.IsMatch(txtMail.Text))
+            {
+                MessageBox.Show("Mail NON valida!", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMail.Text = "";
+                return;
+            }
+            MessageBox.Show("Dati inseriti CORRETTAMENTE", "Operazione Riuscita", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            txtMatricola.Text = txtNome.Text = txtCognome.Text = txtMail.Text = "";
         }
     }
 }
