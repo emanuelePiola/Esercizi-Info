621ecce7d6e25de9984221bf4b72b0dd8c533f0a
emanuelePiola <124259956+emanuelePiola@users.noreply.github.com>
2024-02-25 16:19:18 +0100

    Es01Interfaccie done

---
+++ EsInterfaccie01/EsInterfaccie01/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsInterfaccie01
+{
+    public partial class Form1 : Form
+    {
+        BookStore bookStore = new BookStore();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void btnVenduto_Click(object sender, EventArgs e)
+        {
+            IBook book = new Book(txtTitolo.Text, txtAutore.Text, Convert.ToDouble(txtPrezzo.Text));
+            bookStore.SellBook(book);
+        }
+
+        private void btnVendite_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Totale vendite: " + bookStore.CalculateTotalSales() + "€");
+        }
+
+        private void btnLibriVenduti_Click(object sender, EventArgs e)
+        {
+            bookStore.DisplayBooksSold();
+        }
+    }
+}
---
+++ EsInterfaccie01/EsInterfaccie01/IBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsInterfaccie01
+{
+    internal interface IBook
+    {
+        string Titolo { get; }
+        string Autore { get; }
+        double Prezzo { get; }
+    }
+}
